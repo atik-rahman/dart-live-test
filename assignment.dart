5d76e713de5af2696a7faa7de96f4460f3458f84
Atikur Rahman <atik-rahman@outlook.com>
2024-03-16 01:47:15 +0600

    Added Assignment file

---
+++ assignment.dart
@@ -0,0 +1,30 @@
+class Media{
+  void play(){
+    print("Playing Media...");
+  }
+}
+
+class Song extends Media {
+  String artist;
+
+  //Song Constructor
+  Song(this.artist);
+
+  @override
+  void play(){
+    print("Playing song by $artist....");
+  }
+}
+
+void main(){
+  //New instance of Media
+  Media newMedia = Media();
+
+  //Calling the play method from the Media
+  media.play(); //will print "Playing media..."
+
+  //New instance of Song
+  Song currentlyPlaying = Song("Michael Bolton");
+  currentlyPlaying.play();  // will print "Playing song by Michael Bolton...."
+
+}
